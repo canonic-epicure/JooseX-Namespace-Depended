2736b4fb41730327dc4e3a16942a0c44ad826510
SamuraiJack <nickolay8@gmail.com>
2009-09-08 11:36:27 +0400

    - improved test coverage

--- t/070_dependencies_in_my.t.js
+++ t/070_dependencies_in_my.t.js
@@ -8,7 +8,7 @@ StartTest(function(t) {
     
 //    JooseX.Namespace.Depended.Manager.my.disableCaching = false
     
-    t.plan(5)
+    t.plan(18)
 	
     //==================================================================================================================================================================================
     //t.diag("Specifiying dependency in 'my' singleton")
@@ -49,8 +49,64 @@ StartTest(function(t) {
         t.ok(WithDependedMy, "WithDependedMy was created")
         t.ok(WithDependedMy.my, ".. and it has a singleton")
         
+        t.ok(WithDependedMy.my.result() == 'withDependedMy', "And it has correct 'result' method")
         
         t.endAsync(async2)
     })
+    
+    //==================================================================================================================================================================================
+    //t.diag("Stressed test")
+    
+    var async3 = t.beginAsync()
+    
+    Class('MyMixed', {
+        
+        use : [ 'Chain1.Middle.Chain2', 'WithDependedMy2'],
+    
+        body : function () {
+            t.diag("Stressed test, with mixed dependencies in usual class and 'my' singletons")
+            
+            t.ok(typeof MyMixed == 'function', "Class was created")
+            
+            //Chain1.Middle.Chain2 checkin
+            t.ok(typeof Chain1.Middle.Chain2 == 'function', "Chain1.Middle.Chain2 was created")
+            t.ok(new Chain1.Middle.Chain2().result() == 2, "... and it works correctly")
+            
+            
+            //WithDependedMy2 checkin            
+            t.ok(WithDependedMy2, "Class 'WithDependedMy2' was created")
+            
+            var withDep2 = new WithDependedMy2()
+            
+            t.ok(withDep2.res2 == 'res2', "And it has composed 'res2' attribute")
+            
+            withDep2.process2()
+            
+            t.ok(withDep2.res2 == 'processed2', "And it has composed 'process2' method")
+            
+            
+            //WithDependedMy2.my checkin
+            var my = WithDependedMy2.my
+            
+            t.ok(my, "Class 'WithDependedMy2' has singleton")
+            t.ok(my.result() == 'withDependedMy2', "And it has native 'result' method")
+            
+            t.ok(typeof Chain1 == 'function', "'Chain1' was created")
+            
+            
+            t.ok(my.sugar == 'sweet', "'WithDependedMy2.my' has a correct 'sugared' attribute via additional builder")
+            
+            
+            t.ok(my.res3 == 'res3', "And it has composed 'res3' attribute")
+            
+            my.process3()
+            
+            t.ok(my.res3 == 'processed3', "And it has composed 'process3' method")
+            
+            
+            t.endAsync(async3)
+        }
+    })
+    
     
 })
